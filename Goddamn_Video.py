cb701606990b40ac3b3fae67f7662755845bfebc
CartoonVideoTest <gytc163@163.com>
2025-04-30 09:50:26 +0800

    Update Goddamn_Video.py
    增加提示语，别TM的相信视频内的广告，小心骗得你裤衩子都不剩

--- Goddamn_Video.py
+++ Goddamn_Video.py
@@ -12,7 +12,7 @@ if 'seach' not in st.session_state:
 
 with st.sidebar:
     st.title("Goddamn Video")
-    # st.write("<看视频，用超简洁的Goddamn Video>")
+    st.write("<看视频，用超简洁的Goddamn Video>")
     st.write("<温馨提示：别TM的相信视频内的广告，小心骗得你裤衩子都不剩>")
     videoName=st.text_input("输入影视名称",key='search')
 
@@ -23,6 +23,7 @@ with st.sidebar:
         st.session_state.seach = True
 
 st.write("---")
+st.write("老子再TM说一遍：别TM的相信视频内的广告")
 
 if st.session_state.seach:
 
