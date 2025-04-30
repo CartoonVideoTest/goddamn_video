39fc6c5685c4959c421c3a82cd6bba3e6eacaa24
CartoonVideoTest <gytc163@163.com>
2025-04-30 09:08:49 +0800

    Update Goddamn_Video.py

--- Goddamn_Video.py
+++ Goddamn_Video.py
@@ -13,7 +13,8 @@ if 'seach' not in st.session_state:
 # st.sidebar.write("动漫梦工厂")
 st.title("Goddamn Video")
 st.write("<看视频，用超简洁的Goddamn Video>")
-st.write("<出自千海之手>")
+st.write("<温馨提示：TM的别相信视频内的广告什么的>")
+st.write("<小心骗得你裤衩子都不剩>")
 videoName=st.text_input("输入影视名称",key='search')
 
 if st.button("搜索"):
