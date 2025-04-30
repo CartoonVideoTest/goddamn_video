def79a14f7c66cf44ab73ea33049bc1063e3c71e
CartoonVideoTest <gytc163@163.com>
2025-04-30 11:06:59 +0800

    Update Goddamn_Video.py

--- Goddamn_Video.py
+++ Goddamn_Video.py
@@ -2,9 +2,10 @@ import re
 import requests
 from bs4 import BeautifulSoup
 import streamlit as st
+from pandas.core.dtypes.inference import is_number
 
-header='https://www.tv121.com/'
-header1='https://www.tv121.com/static/js/player.html?url='
+header = 'https://www.tv121.com/'
+header1 = 'https://www.tv121.com/static/js/player.html?url='
 
 # 初始化 session_state
 if 'seach' not in st.session_state:
@@ -13,25 +14,25 @@ if 'seach' not in st.session_state:
 with st.sidebar:
     st.title("Goddamn Video")
     st.write("<看视频，用超简洁的Goddamn Video>")
-    videoName=st.text_input("输入影视名称",key='search')
+    videoName = st.text_input("输入影视名称", key='search')
 
     if st.button("搜索"):
         st.session_state.seach = True
-    
+
     st.write("---")
-    st.write('如有侵权，可联系邮件\n\n<gytc163@163.com>')
-    
+    st.write('如有侵权，可联系邮件\n\n< gytc163@163.com >')
+
 st.write("---")
 st.write("温馨提示：\n\n请勿相信视频内的广告，小心骗得你裤衩子都没得")
 st.write("---")
 
 if st.session_state.seach:
 
-    response=requests.get(f"https://www.tv121.com/sow/{videoName}----------1---.html")
+    response = requests.get(f"https://www.tv121.com/sow/{videoName}----------1---.html")
 
-    soup=BeautifulSoup(response.text,"lxml")
+    soup = BeautifulSoup(response.text, "lxml")
 
-    results=soup.select('div.thumb>a')
+    results = soup.select('div.thumb>a')
 
     num = 1
     for url in results:
@@ -39,7 +40,11 @@ if st.session_state.seach:
         with col1:
             col3, col4 = st.columns(2)
             with col3:
-                st.image(url.get('data-original'))
+                try:
+                    st.image(url.get('data-original'))
+                except:
+                    st.image('./nvwa.jpg')
+                    st.write('图像出问题了，只能拿来充数了')
         with col2:
             name = url.get('title')
             url = header + url.get('href')
@@ -51,31 +56,39 @@ if st.session_state.seach:
 
             li_list = []
             for li_url in li_urls:
-                li_list.append(header+li_url.get('href'))
+                li_list.append(header + li_url.get('href'))
 
             st.write(f"共{len(li_list)}集")
-            if len(li_list)!=1:
-                input_num=st.text_input('选集：',key=f'key{num}')
+            if len(li_list) != 1:
+                input_num = st.text_input('选集（0为最后一集）：', key=f'key{num}')
             else:
-                input_num=1
+                input_num = 1
 
-            if st.button('获取',key=f'input_key{num}'):
-                input_url = li_list[int(input_num) - 1]
+            if st.button('获取', key=f'input_key{num}'):
+                if input_num.isdigit():
 
-                response = requests.get(input_url)
-                soup = BeautifulSoup(response.text, 'lxml')
+                    if int(input_num) > len(li_list):
+                        st.write(f'一共才{len(li_list)}集，你搞什么！！！')
+                    elif int(input_num) < 0:
+                        st.write('输入正整数，就是1，2，3，4……')
+                    else:
+                        input_url = li_list[int(input_num) - 1]
 
-                aa=re.search(r'\+"https://v(.+?).m3u8',response.text).group(0).replace('+"','')
+                        response = requests.get(input_url)
+                        soup = BeautifulSoup(response.text, 'lxml')
 
-                play_url=header1+aa
-                iframe_html =f"""
-                <iframe src="{play_url}" width="300" height="150" frameborder="0" allowfullscreen></iframe>
-                """
+                        aa = re.search(r'\+"https://v(.+?).m3u8', response.text).group(0).replace('+"', '')
 
-                st.markdown(iframe_html, unsafe_allow_html=True)
+                        play_url = header1 + aa
+                        iframe_html = f"""
+                        <iframe src="{play_url}" width="300" height="150" frameborder="0" allowfullscreen></iframe>
+                        """
 
+                        st.markdown(iframe_html, unsafe_allow_html=True)
+                else:
+                    st.write('输入正整数，就是1，2，3，4……')
 
         st.write('---')
 
-        num+=1
-
+        num += 1
+print("搜索内容："+videoName)
